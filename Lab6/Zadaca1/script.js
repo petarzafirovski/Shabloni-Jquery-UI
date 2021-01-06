8e4b503cad4fd50db8241eb34dd3f8670093f9db
Petar Zafirovski <zafirovskipetar@gmail.com>
2021-01-06 12:02:16 +0100

    Lab6

---
+++ Lab6/Zadaca1/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function (){
+    $("#error").hide();
+    $("#errorPass").hide();
+    $("#dialog").dialog({
+        autoOpen:true,
+        modal:true,
+        buttons:{
+            OK:function (){
+                var pas = $("#pas1").val();
+                if(pas.length>=6 && pas.match(/(?=.*[!@#$%^&*])/) && pas===$("#pas2").val())
+                {
+                    $("#dialog").dialog( "close" );
+                }
+                else{
+                    $("#error").show();
+                }
+
+
+            }
+        }
+    })
+
+    $("#dialog2").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            Insert:function (){
+                $("#contentDIV").append("<span>"+$("#contentInp").val()+"</span>");
+                $("#dialog2").dialog( "close" );
+            },
+            Clear:function (){
+                $("#toWho").val("");
+                $("#cc").val("");
+                $("#subject").val("");
+                $("#contentInp").val("");
+                $("#p1").find("span").remove();
+                $("#p2").find("span").remove();
+                $("#p3").find("span").remove();
+                $("#contentDIV").find("span").remove();
+                $("#dialog2").dialog( "close" );
+            }
+        }
+    })
+
+    $("#insertBtn").click(function (){
+        $("#p1").append("<span>"+$("#toWho").val()+"</span>");
+        $("#p2").append("<span>"+$("#cc").val()+"</span>");
+        $("#p3").append("<span>"+$("#subject").val()+"</span>");
+        $("#dialog2").dialog("open");
+    })
+
+    $("#dialog3").dialog({
+        autoOpen:false,
+        modal:true,
+        buttons:{
+            OK:function (){
+                if ($("#pas1").val()===$("#passwordValidation").val()){
+                    $("#dialog3").dialog( "close" );
+                    $(".passValidation").val("");
+
+                }
+                else{
+                    $("#errorPass").show();
+                }
+
+            }
+        }
+    })
+    $("#sendBtn").click(function (){
+        $("#dialog3").dialog("open");
+    })
+})
---
+++ Lab6/Zadaca2/script.js
@@ -0,0 +1,110 @@
+$(document).ready(function (){
+    $("#tab2").hide();
+    $("#tab3").hide();
+    $("#tab1").show();
+    var index=1;
+    $("#next").click(function (){
+        if(index<3){
+            index++;
+        }else if(index===3){
+            return;
+        }
+        $("#id"+index).css("background","darkorange");
+        $("#id"+parseInt(index-1)).css("background","gray");
+        $(".panel").hide();
+        $("#tab"+index).show();
+
+    })
+
+    $("#prev").click(function (){
+        if(index>1){
+            index--;
+        }
+        $("#id"+index).css("background","darkorange");
+        $("#id"+parseInt(index+1)).css("background","steelblue");
+        $(".panel").hide();
+        $("#tab"+index).show();
+    })
+
+    $("#error").hide();
+    $("#errorPass").hide();
+    $("#dialog").dialog({
+        autoOpen:true,
+        modal:true,
+        buttons:{
+            OK:function (){
+                var pas = $("#pas1").val();
+                if(pas.length>=6 && pas.match(/(?=.*[!@#$%^&*])/) && pas===$("#pas2").val())
+                {
+                    $("#dialog").dialog( "close" );
+                }
+                else{
+                    $("#error").show();
+                }
+
+
+            }
+        }
+    })
+
+    $("#dialog2").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            Insert:function (){
+                $("#contentDIV").append("<p>"+$("#contentInp").val()+"</p>");
+                $("#dialog2").dialog( "close" );
+            },
+            Clear:function (){
+                $("#toWho").val("");
+                $("#cc").val("");
+                $("#subject").val("");
+                $("#contentInp").val("");
+                $("#p1").find("span").remove();
+                $("#p2").find("span").remove();
+                $("#p3").find("span").remove();
+                $("#contentDIV").find("p").remove();
+                $("#dialog2").dialog( "close" );
+            }
+        }
+    })
+
+    $("#insertBtn").click(function (){
+        $("#p1").append("<span>"+$("#toWho").val()+"</span>");
+        $("#p2").append("<span>"+$("#cc").val()+"</span>");
+        $("#p3").append("<span>"+$("#subject").val()+"</span>");
+        $("#dialog2").dialog("open");
+    })
+
+    $("#dialog3").dialog({
+        autoOpen:false,
+        modal:true,
+        buttons: {
+            OK: function () {
+                if ($("#pas1").val() === $("#passwordValidation").val()) {
+                    $("#dialog3").dialog("close");
+                    $(".passValidation").val("");
+                    $("#selectable").append("<li class='selected'>" + "<div>" + "To: " + $("#toWho").val() + "<br>" + "Subject: " + $("#subject").val() + "</div>" + "</li>")
+                } else {
+                    $("#errorPass").show();
+                }
+            }
+        }
+    })
+
+    $("#sendBtn").click(function (){
+        $("#dialog3").dialog("open");
+    })
+
+    $("#selectable").selectable();
+
+    $("#deleteBtn").click(function (){
+        $("li.ui-selected").remove();
+    })
+
+    $(".selectable").scroll();
+
+})
+
+
+
